8df94c6f075c0b57df08a3e209515a8a835f0f88
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-10-28 02:12:50 -0500

    aded get endpoints

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = rquire('mongoose');
 
-const blogPostsRouter = require('./blogPostsRouter');
+const {DATABASE_URL, PORT} = require('./config');
+const {BlogPost} = require('./models');
 const app = express();
 
 
 app.use(morgan('common'));
+app.use(bodyParser.json());
 
-// you need to import `blogPostsRouter` router and route
-// requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
-app.use('/blog-posts', blogPostsRouter);
+mongoos.Promise = global.Promise;
+
 
 let server;
+
+app.get('/posts', (req, res) => {
+    BlogPost
+      .find()
+      .then(posts => {
+        res.json(posts.map(post => post.apiRepr()));
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'something went terribly wrong'});
+      });
+  });
+
+  app.get('/posts/:id', (req, res) => {
+    BlogPost
+      .findById(req.params.id)
+      .then(post => res.json(post.apiRepr()))
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'something went horribly awry'});
+      });
+  });
+  
 
 function runServer() {
     const port = process.env.PORT || 8080;
