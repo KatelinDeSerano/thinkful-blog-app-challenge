2a1dd7ec33d660778dd501c591a142d21d19f168
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-10-28 02:45:02 -0500

    added put/delete endpoints, and mongoose configuration

--- server.js
+++ server.js
@@ -63,29 +63,83 @@ app.get('/posts', (req, res) => {
   
   });
 
+  app.delete('/posts/:id', (req, res) => {
+    BlogPost
+      .findByIdAndRemove(req.params.id)
+      .then(() => {
+        res.status(204).json({message: 'success'});
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'something went terribly wrong'});
+      });
+  });
+  
+  app.put('/posts/:id', (req, res) => {
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+      res.status(400).json({
+        error: 'Request path id and request body id values must match'
+      });
+    }
+  
+    const updated = {};
+    const updateableFields = ['title', 'content', 'author'];
+    updateableFields.forEach(field => {
+      if (field in req.body) {
+        updated[field] = req.body[field];
+      }
+    });
+  
+    BlogPost
+      .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+      .then(updatedPost => res.status(204).end())
+      .catch(err => res.status(500).json({message: 'Something went wrong'}));
+  });
+
+  app.delete('/:id', (req, res) => {
+    BlogPosts
+      .findByIdAndRemove(req.params.id)
+      .then(() => {
+        console.log(`Deleted blog post with id \`${req.params.ID}\``);
+        res.status(204).end();
+      });
+  });
+  
+  
+  app.use('*', function(req, res) {
+    res.status(404).json({message: 'Not Found'});
+  });
   
 
+
 function runServer() {
-    const port = process.env.PORT || 8080;
+    const port = (databaseUrl=DATABASE_URL, port=PORT) {
     return new Promise((resolve,reject) => {
-        server = app.listen(port, () => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+           
+            }
+            server = app.listen(port, () => {
             console.log(`Your app is listening on port ${port}`);
-            resolve(server);
-        }).on('error', err => {
-            reject(err)
+            resolve();
+            }).on('error', err => {
+                reject(err);
+            });
         });
     });
 }
 
 function closeServer() {
-    return new Promise((resolve, reject) => {
-      console.log('Closing server');
-      server.close(err => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve();
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+            return reject(err);
+            }
+            resolve();
+        });
       });
     });
   }
